fd9d932c2b44bfd345c4368938a0dd453c7e1c5e
tanzinsunni <131749626+tanzinsunni@users.noreply.github.com>
2023-11-19 19:08:11 +0600

    Update and rename UserController.php to ProfileController.php

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Response;
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+       
+        $name = "Donald Trump";
+        $age = "75";
+
+     
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age
+        ];
+
+        $value = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+      
+        $response = new Response(json_encode($data), 200);
+
+       
+        $response->cookie('access_token', $value, $minutes, $path, $domain, $secure, $httpOnly);
+
+        return $response;
+    }
+}
+
+
+
+
